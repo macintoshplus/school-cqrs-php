edacc3929e0d94ceaa89dbe6f641ee2598b6f710
Jean-Baptiste NAHAN <jean-baptiste.nahan@inextenso.fr>
2014-09-01 14:27:14 +0200

    add custom identity map

--- src/Jbnahan/Bundle/EventStoreBundle/DependencyInjection/JbnahanEventStoreExtension.php
+++ src/Jbnahan/Bundle/EventStoreBundle/DependencyInjection/JbnahanEventStoreExtension.php
@@ -24,5 +24,7 @@ class JbnahanEventStoreExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+		
+		$container->setAlias('litecqrs.identity_map', 'jbnahan.identitymap.orm');
     }
 }
---
+++ src/Jbnahan/ES/DoctrineIdentityMap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jbnahan\ES;
+
+use LiteCQRS\EventProviderInterface;
+
+class DoctrineIdentityMap implements IdentityMapInterface
+{
+    private $map = array();
+
+    public function add(EventProviderInterface $object)
+    {
+        $this->map[spl_object_hash($object)] = $object;
+    }
+    public function all()
+    {
+        return array_values($this->map);
+    }
+    public function getAggregateId(EventProviderInterface $object)
+    {
+        return $object->getId();
+    }
+}
+
+
