4bdc5a996f645affe1849ecda0db8d87dca62919
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2014-09-01 15:01:32 +0200

    Change JMS Serializer init

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             
             new LiteCQRS\Plugin\SymfonyBundle\LiteCQRSBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             
             new Jbnahan\Bundle\EventStoreBundle\JbnahanEventStoreBundle(),
             new Jbnahan\Bundle\SchoolBundle\JbnahanSchoolBundle(),
